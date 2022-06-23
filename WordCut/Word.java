559dda833d1506870fdae2e9e5b738ab277286f3
mushan <102899234+xsb-mushan@users.noreply.github.com>
2022-06-23 20:32:04 +0800

    Create Word.java

---
+++ WordCut/Word.java
@@ -0,0 +1,65 @@
+package com.mushan.WordCut;
+
+public class Word {
+    private String word;
+    private String s1,s2,s3,s4;
+    private String answer;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    private int state;
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getS1() {
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+    public String getS2() {
+        return s2;
+    }
+
+    public void setS2(String s2) {
+        this.s2 = s2;
+    }
+
+    public String getS3() {
+        return s3;
+    }
+
+    public void setS3(String s3) {
+        this.s3 = s3;
+    }
+
+    public String getS4() {
+        return s4;
+    }
+
+    public void setS4(String s4) {
+        this.s4 = s4;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}

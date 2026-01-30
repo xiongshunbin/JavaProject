365f8363547e78700add426d31645a895a44af37
mushan <xiongshunbin@outlook.com>
2026-01-31 01:30:34 +0800

    2026.1.31

--- weblog/weblog-springboot/weblog-web/src/main/java/com/mushan/weblog/web/controller/TestController.java
+++ weblog/weblog-springboot/weblog-web/src/main/java/com/mushan/weblog/web/controller/TestController.java
@@ -5,6 +5,8 @@ import com.mushan.weblog.common.enums.ResponseCodeEnum;
 import com.mushan.weblog.common.exception.BizException;
 import com.mushan.weblog.common.utils.Response;
 import com.mushan.weblog.web.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,10 +20,12 @@ import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
+    @ApiOperation(value = "测试接口")
     public Response test(@RequestBody @Validated User user) {
         /*
         // 是否存在校验错误
--- weblog/weblog-springboot/weblog-web/src/main/java/com/mushan/weblog/web/model/User.java
+++ weblog/weblog-springboot/weblog-web/src/main/java/com/mushan/weblog/web/model/User.java
@@ -1,5 +1,7 @@
 package com.mushan.weblog.web.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.*;
@@ -9,22 +11,27 @@ import javax.validation.constraints.*;
  * @description: TODO
  */
 @Data
+@ApiModel(value = "用户实体类")
 public class User {
     // 用户名
     @NotBlank(message = "用户名不能为空")  // 注解确保用户名不为空
+    @ApiModelProperty(value = "用户名")
     private String username;
     // 性别
     @NotNull(message = "性别不能为空")    // 注解确保性别不为空
+    @ApiModelProperty(value = "用户性别")
     private Integer sex;
 
     // 年龄
     @NotNull(message = "年龄不能为空")
     @Min(value = 18, message = "年龄必须大于或等于 18")  // 注解确保年龄大于等于18
     @Max(value = 100, message = "年龄必须小于或等于 100")    // 注解确保年龄小于等于100
+    @ApiModelProperty(value = "年龄")
     private Integer age;
 
     // 邮箱
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确") // 注解确保邮箱格式正确
+    @ApiModelProperty(value = "邮箱")
     private String email;
 }
